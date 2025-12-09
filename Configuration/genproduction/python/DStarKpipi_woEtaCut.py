6a9faf6ff7247f20803fa759b6a5e062c7fc7f91
JunseokLee3609 <jun502s@korea.ac.kr>
2025-12-09 07:01:26 +0100

    update genfrag

--- Configuration/genproduction/python/DStarKpipi_woEtaCut.py
+++ Configuration/genproduction/python/DStarKpipi_woEtaCut.py
@@ -54,13 +54,13 @@ End
         pythia8CP5SettingsBlock,
         processParameters = cms.vstring(
 	    "SoftQCD:nonDiffractive = on",
-            'PTFilter:filter = on', 
-            'PTFilter:quarkToFilter = 4', 
+            'PTFilter:filter = on',
+            'PTFilter:quarkToFilter = 4',
             'PTFilter:scaleToFilter = 1.0'
 	    ),
         parameterSets = cms.vstring(
-	    'pythia8CommonSettings', 
-            #'pythia8CUEP8M1Settings', 
+	    'pythia8CommonSettings',
+            #'pythia8CUEP8M1Settings',
             'pythia8CP5Settings',
             'processParameters')
     )
@@ -69,21 +69,22 @@ End
 from GeneratorInterface.Core.ExternalGeneratorFilter import ExternalGeneratorFilter
 generator = ExternalGeneratorFilter(_generator)
 
-### Filters 
+### Filters
 
 
 dfilter = cms.EDFilter(
     "MCSingleParticleFilter",
-    MaxEta = cms.untracked.vdouble(99,99),
-    MinEta = cms.untracked.vdouble(-99,-99),
+    MaxEta = cms.untracked.vdouble(1.2,1.2),
+    MinEta = cms.untracked.vdouble(-1.2,-1.2),
+    PtMin = cms.vdouble(2.0,2.0),
     ParticleID = cms.untracked.vint32(413,-413)
 )
 
 d0filter = cms.EDFilter(
     "MCMultiParticleFilter",
     AcceptMore = cms.bool(True),
-    EtaMax = cms.vdouble(99, 99),
-    EtaMin = cms.vdouble(-99, -99),
+    EtaMax = cms.vdouble(2.8, 2.8),
+    EtaMin = cms.vdouble(-2.8, -2.8),
     MotherID = cms.untracked.vint32(421),
     NumRequired = cms.int32(1),
     ParticleID = cms.vint32(-321, 211),
@@ -98,9 +99,9 @@ decayfilter = cms.EDFilter(
     NumberDaughters = cms.untracked.int32(2),
     ParticleID      = cms.untracked.int32(413),  ## DStar+ (already chage conjugate)
     DaughterIDs     = cms.untracked.vint32(421,211), ## D0 and pi+
-#    MinPt           = cms.untracked.vdouble( 0. ,  0.4), ## cuts based on data
-    MinEta          = cms.untracked.vdouble(-99., -99.), ## cuts based on data
-    MaxEta          = cms.untracked.vdouble( 99.,  99.) ## cuts based on data
+    MinPt           = cms.untracked.vdouble( 0.0 ,  0.0), ## cuts based on data
+    MinEta          = cms.untracked.vdouble(-2.8, -2.8), ## cuts based on data
+    MaxEta          = cms.untracked.vdouble( 2.8,  2.8) ## cuts based on data
 )
 
 
